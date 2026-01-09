f6cda63febc75ed1799f5835d62d4a729ea51228
ricardovsoares <ricardovsoares@gmail.com>
2026-01-09 09:29:46 -0300

    Ajuste em database.py para recuperar o DATABASE_URL de acordo com a configuração do ambiente

--- utils/database.py
+++ utils/database.py
@@ -2,7 +2,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine, create_async_engine
 from utils.configs import settings
 
-engine: AsyncEngine = create_async_engine(settings.DB_URL, echo=True)
+# engine: AsyncEngine = create_async_engine(settings.DB_URL, echo=True)
+
+engine: AsyncEngine = create_async_engine(
+    settings.effective_db_url,
+    connect_args=settings.sqlalchemy_connect_args,
+    echo=True,
+    pool_pre_ping=True,
+)
+
 
 Session: AsyncSession = sessionmaker(
     autocommit=False,
